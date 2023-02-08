b58071d77901c7b48fede9f7b6af2eb2594a8d1f
JaggedZirconium <67721548+JaggedZirconium@users.noreply.github.com>
2023-02-07 19:01:43 -0600

    variable renames

--- src/main/java/fleroviumturtle1/debugandtest/item/DebuggingWandItem.java
+++ src/main/java/fleroviumturtle1/debugandtest/item/DebuggingWandItem.java
@@ -12,19 +12,21 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.Objects;
+
 public class DebuggingWandItem extends Item {
-    public DebuggingWandItem(Properties p_41383_) {
-        super(p_41383_);
+    public DebuggingWandItem(Properties properties) {
+        super(properties);
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext useOn) {
-        Level LEVEL = useOn.getLevel();
-        BlockPos POSI = useOn.getClickedPos();
+    public InteractionResult useOn(UseOnContext useon) {
+        Level LEVEL = useon.getLevel();
+        BlockPos POSI = useon.getClickedPos();
         BlockState BS = LEVEL.getBlockState(POSI);
 
         String BLPT = "Block Position: "+Float.toString(POSI.getX())+", "+Float.toString(POSI.getY())+", "+Float.toString(POSI.getZ());
-        useOn.getPlayer().sendSystemMessage(Component.translatable("chat.dat.blockpos",BLPT));
-        return super.useOn(useOn);
+        Objects.requireNonNull(useon.getPlayer()).sendSystemMessage(Component.translatable("chat.dat.blockpos",BLPT));
+        return super.useOn(useon);
     }
 }
